9d3babcd53fef0d1249983e5e0286e6a0f050be7
shanahandnlf <shanahan.danualif@ui.ac.id>
2024-05-14 18:35:32 +0700

    fix app namespace

--- play_podcast/urls.py
+++ play_podcast/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from play_podcast.views import play_podcast
 
-app_name = 'kelola_podcast'
+app_name = 'play_podcast'
 
 urlpatterns = [
     path('play-podcast/', play_podcast, name='play_podcast'),
