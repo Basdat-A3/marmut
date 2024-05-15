69591970f689b0c013a0a65412bbd32b8f193d2d
shanahandnlf <shanahan.danualif@ui.ac.id>
2024-05-16 00:04:48 +0700

    add print psql in log

--- utils/query.py
+++ utils/query.py
@@ -17,6 +17,10 @@ def get_database_cursor():
         # Create a cursor for database operations
         cursor = connection.cursor()
 
+        # Print detail postgre 
+        print("PostgreSQL server information")
+        print(connection.get_dsn_parameters(), "\n")
+
         # Set the search path to the desired schema (e.g., 'marmut')
         cursor.execute("SET search_path TO marmut")
 
@@ -25,39 +29,36 @@ def get_database_cursor():
     except (Exception, Error) as error:
         print("Error while connecting to PostgreSQL:", error)
 
-# try:
-#     # Connect ke db
-#     connection = psycopg2.connect(
-#         user = settings.DATABASES['default']['USER'],
-#         password = settings.DATABASES['default']['PASSWORD'],
-#         host = settings.DATABASES['default']['HOST'],
-#         port = settings.DATABASES['default']['PORT'],
-#         database = settings.DATABASES['default']['NAME'],
-#         # conn max age
-#         # connection pooling
-#         # https://docs.djangoproject.com/en/3.0/ref/databases/#persistent-connections
-#         # https://docs.djangoproject.com/en/3.0/ref/databases/#connection-pooling
-#         # https://docs.djangoproject.com/en/3.0/ref/settings/#conn-max-age
-        
+try:
+    # Connect ke db
+    connection = psycopg2.connect(
+        user = settings.DATABASES['default']['USER'],
+        password = settings.DATABASES['default']['PASSWORD'],
+        host = settings.DATABASES['default']['HOST'],
+        port = settings.DATABASES['default']['PORT'],
+        database = settings.DATABASES['default']['NAME'],
+        # connection pooling
+        # https://docs.djangoproject.com/en/3.0/ref/databases/#persistent-connections
+        # https://docs.djangoproject.com/en/3.0/ref/databases/#connection-pooling
+        # https://docs.djangoproject.com/en/3.0/ref/settings/#conn-max-age
+    )
 
-#     )
+    # Buat cursor buat operasiin db
+    cursor = connection.cursor()
 
-#     # Buat cursor buat operasiin db
-#     cursor = connection.cursor()
+    # Print detail postgre 
+    print("PostgreSQL server information")
+    print(connection.get_dsn_parameters(), "\n")
 
-#     # Print detail postgre 
-#     print("PostgreSQL server information")
-#     print(connection.get_dsn_parameters(), "\n")
+    # Coba query
+    cursor.execute("SELECT version();")
 
-#     # Coba query
-#     cursor.execute("SELECT version();")
+    # Fetch result
+    record = cursor.fetchall()
+    print("You are connected to - ", record, "\n")
 
-#     # Fetch result
-#     record = cursor.fetchall()
-#     print("You are connected to - ", record, "\n")
-
-#     # Masuk ke schema marmut
-#     cursor.execute("SET search_path TO marmut")
+    # Masuk ke schema marmut
+    cursor.execute("SET search_path TO marmut")
   
-# except (Exception, Error) as error:
-#     print("Error while connecting to PostgreSQL", error)
+except (Exception, Error) as error:
+    print("Error while connecting to PostgreSQL", error)
