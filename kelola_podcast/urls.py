822ccab6685c6910fc2cba74097222e81bd56c63
shanahandnlf <shanahan.danualif@ui.ac.id>
2024-05-16 13:05:36 +0700

    make post podcast and episode

--- kelola_podcast/urls.py
+++ kelola_podcast/urls.py
@@ -6,7 +6,7 @@ app_name = 'kelola_podcast'
 urlpatterns = [
     path('create-podcast/', create_podcast, name='create_podcast'),
     path('list-podcast/', list_podcast, name='list_podcast'),
-    path('create-episode/', create_episode, name='create_episode'),
+    path('create-episode/<uuid:podcast_id>/', create_episode, name='create_episode'),
     # path('daftar-episode/', daftar_episode, name='daftar_episode'),
     path('daftar-episode/<uuid:podcast_id>/', daftar_episode, name='daftar_episode'),
 ]
