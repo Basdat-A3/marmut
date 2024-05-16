f5904e7cb25469534956c73dd1cdb08216e558e3
nadinezhr <dyandrandnzahira@gmail.com>
2024-05-17 03:23:18 +0700

    CRUD kelola playlist belum selesai

--- kelola_playlist/urls.py
+++ kelola_playlist/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from . import views
-from kelola_playlist.views import playlist, playlist_detail, tambah_lagu,tambah_playlist
+from kelola_playlist.views import playlist, playlist_detail, tambah_lagu,tambah_playlist, delete_playlist, edit_playlist
 
 app_name = 'kelola_playlist'
 
 urlpatterns = [
     path('playlist/', playlist, name='playlist'),
-    path('playlist-detail/', playlist_detail, name='detail_playlist'),
-    path('tambah-lagu/', tambah_lagu, name='tambah_lagu'),
-    path('tambah-playlist/', tambah_playlist, name='tambah_playlist'),
+    path('playlist_detail/<uuid:idPlaylist>/', playlist_detail, name='playlist_detail'),
+    path('tambah_lagu/', tambah_lagu, name='tambah_lagu'),
+    path('tambah_playlist/', tambah_playlist, name='tambah_playlist'),
+    path('delete_playlist/<uuid:idPlaylist>/', delete_playlist, name='delete_playlist'),
+    path('edit_playlist/<uuid:idPlaylist>/', edit_playlist, name='edit_playlist'),
 ]
 
