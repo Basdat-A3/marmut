ccc584ced84579c6e45e824456f9e12e785de2ba
nadinezhr <dyandrandnzahira@gmail.com>
2024-05-16 21:00:59 +0700

    extends navbar

--- kelola_playlist/urls.py
+++ kelola_playlist/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-
 from . import views
+from kelola_playlist.views import playlist, playlist_detail, tambah_lagu,tambah_playlist
+
 app_name = 'kelola_playlist'
 
-
 urlpatterns = [
-    path('playlist/', views.playlist, name='playlist'),
-    # path('create_podcast/', views.create_podcast),
+    path('playlist/', playlist, name='playlist'),
+    path('playlist-detail/', playlist_detail, name='detail_playlist'),
+    path('tambah-lagu/', tambah_lagu, name='tambah_lagu'),
+    path('tambah-playlist/', tambah_playlist, name='tambah_playlist'),
 ]
+
--- kelola_playlist/views.py
+++ kelola_playlist/views.py
@@ -1,5 +1,45 @@
 from django.shortcuts import render
-
+from utils.query import *
 
 def playlist(request):
+    connection, cursor = get_database_cursor()
+    
+    # close connection
+    cursor.close()
+    connection.close()
+
+    
     return render(request, 'playlist.html')
+
+def playlist_detail(request):
+    connection, cursor = get_database_cursor()
+
+
+    # close connection
+    cursor.close()
+    connection.close()
+
+
+    return render(request, 'playlist_detail.html')
+
+def tambah_lagu(request):
+    connection, cursor = get_database_cursor()
+
+
+    # close connection
+    cursor.close()
+    connection.close()
+
+
+    return render(request, 'tambah_lagu.html')
+
+def tambah_playlist(request):
+    connection, cursor = get_database_cursor()
+
+
+    # close connection
+    cursor.close()
+    connection.close()
+
+
+    return render(request, 'tambah_playlist.html')
